cac02c9cbede8e4a446a32a2e01a1fad2304f8e8
Niza Vera <nizavera95@gmail.com>
2020-10-19 17:22:17 +0000

    finished main styling for columns in crm

---
+++ src/js/component/crmContactCard.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect, useContext } from "react";
+import { withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import MikePhoto from "../../img/m101.jpg";
+import { Context } from "../store/appContext";
+
+export const CrmContactCard = props => {
+	const [state, setState] = useState({
+		//initialize state here
+	});
+	const { store, actions } = useContext(Context);
+
+	return (
+		<li className="list-group-item">
+			<div className="card" style={{ width: "100%" }}>
+				<div>
+					<div className="card-body">
+						<h5 className="card-title">{props.full_name}</h5>
+						<p className="card-text">
+							<span
+								className="fa fa-phone fa-fw text-muted mr-3"
+								data-toggle="tooltip"
+								title=""
+								data-original-title="(870) 288-4149"
+							/>
+							<span className="text-muted small">{props.phone}</span>
+							<br />
+							<span
+								className="fa fa-envelope fa-fw text-muted mr-3"
+								data-toggle="tooltip"
+								data-original-title=""
+								title=""
+							/>
+							<span className="text-muted small text-truncate">{props.email}</span>
+						</p>
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+					</div>
+				</div>
+			</div>
+		</li>
+	);
+};
+
+/**
+ * Define the data-types for
+ * your component's properties
+ **/
+CrmContactCard.propTypes = {
+	history: PropTypes.object,
+	onDelete: PropTypes.func,
+	full_name: PropTypes.string,
+	email: PropTypes.string,
+	phone: PropTypes.string,
+	address: PropTypes.string,
+	index: PropTypes.number,
+	match: PropTypes.object,
+	contactId: PropTypes.number
+};
+
+/**
+ * Define the default values for
+ * your component's properties
+ **/
+CrmContactCard.defaultProps = {
+	onDelete: null
+};
--- src/js/views/custManage.js
+++ src/js/views/custManage.js
@@ -2,26 +2,46 @@ import React, { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import PropTypes from "prop-types";
+import { CrmContactCard } from "../component/crmContactCard.js";
 import "../../styles/crm.scss";
 
 export const CustManage = props => {
+	const { store, actions } = useContext(Context);
 	return (
-		<div className="container">
-			<div className="d-flex flex-row justify-content-between">
+		<div className="container uni-height">
+			<div className="d-flex flex-row justify-content-between uni-height">
+				<div className="d-flex flex-column column">
+					<h3 className="border-bottom border-dark text-center pt-2 pb-2 mb-1">In Process</h3>
+
+					<div className="  overflow-auto">
+						<ul className="list-group pull-down" id="contact-list">
+							{store.contacts.map((contact, index) => {
+								return (
+									<CrmContactCard
+										key={index}
+										full_name={contact.full_name}
+										contactId={contact.id}
+										email={contact.email}
+										phone={contact.phone}
+										address={contact.address}
+										index={contact.id}
+										onDelete={() => setState({ showModal: true, id: contact.id })}
+									/>
+								);
+							})}
+						</ul>
+					</div>
+				</div>
 				<div className=" column">
-					<h3 className="border-bottom border-dark text-center">In Process</h3>
+					<h3 className="border-bottom border-dark text-center pt-2 pb-2 mb-1">In Contact</h3>
 					<p>Some text..</p>
 				</div>
 				<div className=" column">
-					<h3 className="border-bottom border-dark text-center">In Contact</h3>
+					<h3 className="border-bottom border-dark text-center pt-2 pb-2 mb-1">Won</h3>
 					<p>Some text..</p>
 				</div>
 				<div className=" column">
-					<h3 className="border-bottom border-dark text-center">Won</h3>
-					<p>Some text..</p>
-				</div>
-				<div className=" column">
-					<h3 className="border-bottom border-dark text-center">Loss</h3>
+					<h3 className="border-bottom border-dark text-center pt-2 pb-2 mb-1">Loss</h3>
 					<p>Some text..</p>
 				</div>
 			</div>
