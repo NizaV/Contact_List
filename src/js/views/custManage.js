402d6aea0f2d264ef79ddc4e51a4db4ec1938351
Niza Vera <nizavera95@gmail.com>
2020-10-19 21:00:08 +0000

    having to go local, smell ya later

--- src/js/views/custManage.js
+++ src/js/views/custManage.js
@@ -7,26 +7,44 @@ import "../../styles/crm.scss";
 
 export const CustManage = props => {
 	const { store, actions } = useContext(Context);
+	const allowDrop = ev => {
+		ev.preventDefault();
+	};
+	const handleDragOver = ev => {
+		ev.dataTransfer.setData("text", ev.target.id);
+	};
+
+	const handleDrop = ev => {
+		ev.preventDefault();
+		var data = ev.dataTransfer.getData("text");
+		ev.inContact.appendChild(document.getElementById(data));
+	};
 	return (
 		<div className="container uni-height">
 			<div className="d-flex flex-row justify-content-between uni-height">
 				<div className="d-flex flex-column column">
 					<h3 className="border-bottom border-dark text-center pt-2 pb-2 mb-1">In Process</h3>
 
-					<div className="  overflow-auto">
-						<ul className="list-group pull-down" id="contact-list">
+					<div className="overflow-auto">
+						<ul
+							className="list-group pull-down"
+							id="inProcess"
+							onDrop={event => handleDragOver(event)}
+							onDragOver={event => allowDrop(event)}>
 							{store.contacts.map((contact, index) => {
 								return (
-									<CrmContactCard
-										key={index}
-										full_name={contact.full_name}
-										contactId={contact.id}
-										email={contact.email}
-										phone={contact.phone}
-										address={contact.address}
-										index={contact.id}
-										onDelete={() => setState({ showModal: true, id: contact.id })}
-									/>
+									<div key={contact.id} draggable="true" onDragStart={event => handleDragOver(event)}>
+										<CrmContactCard
+											key={index}
+											full_name={contact.full_name}
+											contactId={contact.id}
+											email={contact.email}
+											phone={contact.phone}
+											address={contact.address}
+											index={contact.id}
+											onDelete={() => setState({ showModal: true, id: contact.id })}
+										/>
+									</div>
 								);
 							})}
 						</ul>
@@ -34,14 +52,25 @@ export const CustManage = props => {
 				</div>
 				<div className=" column">
 					<h3 className="border-bottom border-dark text-center pt-2 pb-2 mb-1">In Contact</h3>
+					<div className="overflow-auto">
+						<ul
+							className="list-group pull-down"
+							style={{ height: "100%" }}
+							id="inContact"
+							onDrop={event => handleDragOver(event)}
+							onDragOver={event => allowDrop(event)}>
+							{store.contacts.map((contact, index) => {
+								return <div key={contact.id} draggable="true" onDragStart="drag(event)"></div>;
+							})}
+						</ul>
+					</div>
+				</div>
+				<div className=" column">
+					<h3 className="border border-dark text-center pt-2 pb-2 mb-1 bg-success">Won</h3>
 					<p>Some text..</p>
 				</div>
 				<div className=" column">
-					<h3 className="border-bottom border-dark text-center pt-2 pb-2 mb-1">Won</h3>
-					<p>Some text..</p>
-				</div>
-				<div className=" column">
-					<h3 className="border-bottom border-dark text-center pt-2 pb-2 mb-1">Loss</h3>
+					<h3 className="border border-dark text-center pt-2 pb-2 mb-1 bg-danger">Loss</h3>
 					<p>Some text..</p>
 				</div>
 			</div>
